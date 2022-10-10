5b37590063882b72f930cc49ae925ec243676524
saulofernando <saulopauloandrea@gmail.com>
2022-10-10 15:07:05 -0300

    media

---
+++ ex02.py
@@ -0,0 +1,6 @@
+n1 = int(input('digite a 1º nota: '))
+n2 = int(input('digite a 2º nota: '))
+n3 = int(input('digite a 3º nota: '))
+total = n1 + n2 + n3
+media = total / 3
+print('A soma de todas as notas são {} e a média do aluno é de {:.1f}'.format(total,media))
